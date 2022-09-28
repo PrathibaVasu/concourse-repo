96962b1f9f35ad5c8a6da1adc1f7eacff5335ea8
prathibavasu <prathiba.vasu@quantiphi.com>
2022-09-28 17:52:04 +0530

    modified variables

--- terraform/resources/variables.tf
+++ terraform/resources/variables.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ###S3 
 variable "bucket_name" {}
 
@@ -26,32 +25,3 @@ variable "table_name" {}
 variable "table_billing_mode" {}
 
 variable "hash_key" {}
-=======
-###S3 
-variable "bucket_name" {}
-
-variable "acl_value" {}
-
-variable "region" {}
-
-variable "versioning" {}
-
-variable "tags" {}
-
-### Glue variables 
-variable "python_version" {}
-
-variable "no_of_workers" {}
-
-variable "script_location" {}
-
-
-
-###Dynamodb 
-
-variable "table_name" {}
-
-variable "table_billing_mode" {}
-
-variable "hash_key" {}
->>>>>>> 7adaf8f252c708b35524a3c20ddce07b562734f7
