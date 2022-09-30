b3cce4a70fa6cf29f943e8a89df82eb5f14e4d53
prathibavasu <prathiba.vasu@quantiphi.com>
2022-09-30 13:32:18 +0530

    added lambda path2

--- terraform/config/terraform_create.sh
+++ terraform/config/terraform_create.sh
@@ -3,7 +3,7 @@
 cd terraform-resources/
 mkdir ./terraform/temp 
 
-find ./terraform -name '*.tf*' -exec cp {} ./terraform/temp/ \;
+find ./terraform -name '*.tf*' '*.py' -exec cp {} ./terraform/temp/ \;
 cd /terraform/temp
 terraform init 
 terraform plan
