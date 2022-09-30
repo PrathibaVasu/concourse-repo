1765f31f33e9881834691578aaa020a6b312b1a5
prathibavasu <prathiba.vasu@quantiphi.com>
2022-09-30 13:54:07 +0530

    added lambda path2

--- terraform/etl_resources/lambda.tf
+++ terraform/etl_resources/lambda.tf
@@ -1,21 +1,6 @@
 #lambda module    
 
-module "lambda_function" {
-  source = "terraform-aws-modules/lambda/aws"
-#  version = "2.34.0"
 
-  function_name = "terraform-test-function"
-  description   = "My awesome lambda function"
-  handler       = "index.lambda_handler"
-  runtime       = "python3.9"
-
-  source_path = "${path.module}"
-  #source_path = "terraform-resources/terraform/scripts/lambda_scripts/rds_lambda_handler"
-
-  tags = {
-    Name = "my-lambda1"
-  }
-  create_role = false
-  lambda_role = "arn:aws:iam::298841451579:role/AWSPractice-Developer"
+output "pwd" {
+  value = "${path.module}"
 }
-
